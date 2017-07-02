374784952d598b3b16b4f1e5310aeef1b9bb374b
mauq <mauqj3@gmail.com>
2017-07-02 13:36:44 -0700

    blog home toggle works. make sure individual blog posts add to blog url

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 $(function() {
-  function home() {
+
+  $('#home-btn').click(function() {
+    $('#blog-btn').removeClass('active');
+    $(this).addClass('active');
     window.location.href='/';
-    $('#blog').removeClass('active');
-    $(this).addClass('active');
-    console.log('home');
-  }
+  });
 
-  function blog() {
+  $('#blog-btn').click(function() {
+    $('#home-btn').removeClass('active');
+    $(this).addClass('active');
     window.location.href='/blog';
-    $('#home').removeClass('active');
-    $(this).addClass('active');
-    console.log('blog');
-  }
+  });
 
-  $('#home').onclick = home;
-  $('#blog').onclick = blog;
+  if (window.location.href.indexOf('/blog') > -1) {
+      $('#home-btn').removeClass('active');
+        $('#blog-btn').addClass('active');
+  }
 
 });
