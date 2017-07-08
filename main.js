b1373cbe2f210b39d316e33ff6a27ee01f3d2aa8
mauq <mauqj3@gmail.com>
2017-07-07 17:02:33 -0700

    work

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ $(function() {
   $('#blog-btn').click(function() {
     $('#home-btn').removeClass('active');
     $(this).addClass('active');
-    window.location.href='/blog';
+    window.location.href='/portfolio/blog';
   });
 
   if (window.location.href.indexOf('/blog') > -1) {
       $('#home-btn').removeClass('active');
         $('#blog-btn').addClass('active');
   }
-  
+
 });
