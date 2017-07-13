a77e60d17b155e3a97614d1d61497f0d030f943b
mauq <mauqj3@gmail.com>
2017-07-13 13:56:09 -0700

    this should do it

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
 $(function() {
-  //variables
-  var maxDesLen = 150;
-
-  $('#home-btn').click(function() {
-    $('#blog-btn').removeClass('active');
-    $(this).addClass('active');
-    window.location.href='/';
-  });
-
-  $('#blog-btn').click(function() {
-    $('#home-btn').removeClass('active');
-    $(this).addClass('active');
-    window.location.href='/blog-home';
-  });
-
-  if (window.location.href.indexOf('/blog') > -1) {
-      $('#home-btn').removeClass('active');
-        $('#blog-btn').addClass('active');
-  }
 
 });
